eb99d57ee855d3b9db73acaa78af660d8c4b6189
Adrian Lee <your_email@example.com>
2019-06-11 17:22:30 +1000

    added backend module

--- EC2/output.tf
+++ EC2/output.tf
@@ -9,3 +9,7 @@ output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
   value       = aws_instance.vm.*.public_ip
 }
+output "private_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = aws_instance.vm.*.private_ip
+}
